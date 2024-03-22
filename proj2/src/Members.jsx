932410268f4d922fff04b9087520fada17b0c6fe
rkumar06 <riyakumar06@gmail.com>
2024-03-21 23:00:27 -0700

    some member functionality

--- proj2/src/Members.jsx
+++ proj2/src/Members.jsx
@@ -1,15 +1,25 @@
 import React from "react"
 import Polaroid from "./Polaroid"
+import BigPolaroid from "./BigPolaroid"
 import './style.css'
 
 
 function Members(){
-    const memberInfo = [{Name: "test 1", Desc: "bio", Img: "img"}, {Name: "test 1", Desc: "bio", Img: "img"}, {Name: "test 1", Desc: "bio", Img: "img"}]
+    const memberInfo = [{name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}]
+    const memberPolaroids = memberInfo.map(member => <Polaroid name = {member.name} bio = {member.bio} img = {member.img} />)
+    const [isBig, setBig] = React.useState(false)
+
+    function handleX() {
+        setBig(false)
+    }
+    function handlePolaroidClicked(){
+        setBig(true)
+    }
+    
     return (
-        <Polaroid 
-            name = "name"
-            bio = "bio"
-            img = "img"/>
+        <div className = "members-page">
+            {isBig ? <BigPolaroid/> : memberPolaroids}
+        </div>
     )
 }
 
--- proj2/src/Polaroid.jsx
+++ proj2/src/Polaroid.jsx
@@ -3,10 +3,12 @@ import './style.css'
 
 function Polaroid(props){
     return (
-        <div>
-            <h3>{props.img}</h3>
-            <h3>{props.name}</h3>
-            <h4>{props.bio}</h4>
+        <div className = "polaroid-container">
+            <div className="polaroid">
+                <h3>{props.img}</h3>
+                <h3>{props.name}</h3>
+                <h4>{props.bio}</h4>
+            </div>
         </div>
     
     )
