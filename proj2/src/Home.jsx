0acf5372ae6b97c96137a9e5dd0fa8092ae561b8
rkumar06 <riyakumar06@gmail.com>
2024-03-21 22:20:17 -0700

    homepage

--- proj2/src/Home.jsx
+++ proj2/src/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import './style.css'
 
 function Home(){
     return (
-        <h1>home</h1>
+        <div>
+            <img src = "./src/images/HomePageImage.png"  alt = "home image" className = "home-image"/>
+            <h1 className="home-page-title">Hi! We are NoteWorthy A Capella!</h1>
+            <p className="home-paragraph">We’re a lower-voices a cappella group known for our fun performances and fresh takes on popular songs! It always feels sunny when we perform, and not just because of our golden vests!
+                <br/><br/>Since our founding in 2000, we’ve gone million-view-viral on YouTube multiple times, released an album entirely composed of original songs, and performed at the 2022 UC Berkeley commencement.</p>
+        </div>
     )
 }
 
