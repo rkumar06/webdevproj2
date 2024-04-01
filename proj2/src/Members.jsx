236fb0962fc4a82f7d78385b61f2174af8814171
rkumar06 <riyakumar06@gmail.com>
2024-04-01 15:04:30 -0700

    display all info

--- proj2/src/Members.jsx
+++ proj2/src/Members.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import Polaroid from "./Polaroid"
 import BigPolaroid from "./BigPolaroid"
+import memberData from "./memberData.json"
+import { nanoid } from 'nanoid';
 import './style.css'
 
 
 function Members(){
-    const memberInfo = [{name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}]
-    const memberPolaroids = memberInfo.map(member => <Polaroid name = {member.name} bio = {member.bio} img = {member.img} onClick = {handlePolaroidClicked} />)
+    // const memberInfo = [{name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}, {name: "name", bio: "bio", img: "img"}]
+    const memberInfo = memberData.map(mem => ({ key: nanoid(), name: mem.name, bio: mem.bio, pronoun: mem.pronouns}));
+    const memberPolaroids = memberInfo.map(member => <Polaroid name = {member.name} bio = {member.bio} img = {member.img} onClick = {handlePolaroidClicked} pronoun = {member.pronoun}/>)
     const [isBig, setBig] = React.useState(false)
+    // console.log(memberName)
 
     function handleX() {
         setBig(false)
--- proj2/src/Polaroid.jsx
+++ proj2/src/Polaroid.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import './style.css'
 
 function Polaroid(props){
+    const shortenedBio = props.bio.substring(0, 35);
+    console.log(props.pronoun)
     return (
         <div className="polaroid-container">
             <div className="polaroid" onClick = {props.onClick}>
@@ -9,7 +11,8 @@ function Polaroid(props){
                     <h3 className="polaroid-img">{props.img}</h3>
                 </div>
                 <h3 style={{ backgroundColor: "white" }}>{props.name}</h3>
-                <h4 style={{ backgroundColor: "white" }}>{props.bio}</h4>
+                <h4 style={{ backgroundColor: "white" }}>{props.pronoun}</h4>
+                <h5 style={{ backgroundColor: "white" }}>{`${shortenedBio}...`}</h5>
             </div>
         </div>
     )
