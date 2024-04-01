463c25ebb2ae0e1d91843dc658309472ff86a265
Alicia Shanahan <alicia02s@berkeley.edu>
2024-04-01 14:21:57 -0700

    changes

--- proj2/src/Polaroid.jsx
+++ proj2/src/Polaroid.jsx
@@ -3,12 +3,15 @@ import './style.css'
 
 function Polaroid(props){
     return (
-        <div>
-            <h3>{props.img}</h3>
-            <h3>{props.name}</h3>
-            <h4>{props.bio}</h4>
+        <div class="polaroid-container">
+            <div class="polaroid">
+                <div class="polaroid-img-container">
+                    <h3 class="polaroid-img">{props.img}</h3>
+                </div>
+                <h3 style={{ backgroundColor: "white" }}>{props.name}</h3>
+                <h4 style={{ backgroundColor: "white" }}>{props.bio}</h4>
+            </div>
         </div>
-    
     )
 }
 
