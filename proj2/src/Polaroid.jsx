88fbc42e527148fe52da26c08481244e5f03db46
rkumar06 <riyakumar06@gmail.com>
2024-04-01 14:21:22 -0700

    after spring break

--- proj2/src/Polaroid.jsx
+++ proj2/src/Polaroid.jsx
@@ -3,11 +3,13 @@ import './style.css'
 
 function Polaroid(props){
     return (
-        <div className = "polaroid-container">
-            <div className="polaroid">
-                <h3>{props.img}</h3>
-                <h3>{props.name}</h3>
-                <h4>{props.bio}</h4>
+        <div class="polaroid-container">
+            <div class="polaroid">
+                <div class="polaroid-img-container">
+                    <h3 class="polaroid-img">{props.img}</h3>
+                </div>
+                <h3 style={{ backgroundColor: "white" }}>{props.name}</h3>
+                <h4 style={{ backgroundColor: "white" }}>{props.bio}</h4>
             </div>
         </div>
     
