af1b1792f7643059b77d915d74d11ea83692da72
rkumar06 <riyakumar06@gmail.com>
2024-04-01 21:06:12 -0700

    about us bones

--- proj2/src/AboutUs.jsx
+++ proj2/src/AboutUs.jsx
@@ -3,7 +3,54 @@ import React from "react"
 
 function AboutUs() {
     return (
-        <h1>about us</h1>
+        <div className = "aboutus-page">
+            <div className="members-title">
+                <h1>About Us</h1>
+            </div>
+
+            <div className = "about-all-content">
+
+                <h3>Learn more about us and book us for your events! </h3>
+                <br/>
+                <h3>Who We are</h3>
+                <p>blah blah blah</p>
+                <p>blah blah blah</p>
+
+                <div className = "about-events-flyers">
+                    <div className = "about-events-text">
+                        <h3>Events</h3>
+                        <p>We organize Welcome Back to A Cappella, Cal’s a cappella orientation, at the beginning of every semester. Hundreds of students turn out to hear 13+ a cappella groups perform. The show is always fun, and we hope you’ll come!</p>
+                    </div>
+                    <div className = "about-flyers">
+
+                    </div>
+                </div>
+
+
+                <div className = "about-hire-us">
+                    <div className = "about-hire-us-title">
+                    </div> 
+                    <div className = "about-hire-us-pic-paragraph">
+                        <p>We’re no stranger to weddings, private parties, tailgates, and conferences. We’ve performed at venues including San Francisco City Hall, University House, the Faculty Club, and UC Berkeley’s 2022 Commencement.</p>
+
+                    </div>
+                    
+
+                </div>
+                <div className = "about-instagram">
+                    <div className = "about-insta-title">
+
+                    </div>
+                    <div className = "insta-embed">
+
+                    </div>
+
+                </div>
+
+            </div>
+        
+
+        </div>
     )
 }
 
--- proj2/src/Home.jsx
+++ proj2/src/Home.jsx
@@ -4,10 +4,12 @@ import './style.css'
 function Home(){
     return (
         <div>
-            <img src = "./src/images/HomePageImage.png"  alt = "home image" className = "home-image"/>
+        <img src = "./src/images/HomePageImage.png"  alt = "home image" className = "home-image"/>
+        <div className = "home-page">
             <h1 className="home-page-title">Hi! We are NoteWorthy A Capella!</h1>
             <p className="home-paragraph">We’re a lower-voices a cappella group known for our fun performances and fresh takes on popular songs! It always feels sunny when we perform, and not just because of our golden vests!
                 <br/><br/>Since our founding in 2000, we’ve gone million-view-viral on YouTube multiple times, released an album entirely composed of original songs, and performed at the 2022 UC Berkeley commencement.</p>
+        </div>
         </div>
     )
 }
