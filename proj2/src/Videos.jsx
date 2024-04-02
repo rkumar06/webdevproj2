004edd5927134f96760addd5eae6c6f04d9ec21b
Alicia Shanahan <alicia02s@berkeley.edu>
2024-04-01 20:21:46 -0700

    working on videos

--- proj2/src/Videos.jsx
+++ proj2/src/Videos.jsx
@@ -2,7 +2,14 @@ import React from "react"
 
 function Videos(){
     return (
-        <h1>videos</h1>
+        <div className="videos-page">
+            <div className="members-title">
+                <h1>Videos</h1>
+            </div>
+            <div className="videos-body">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/cDjY-1WuB3o?si=uaE3OVfeSOaxRBW-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
     )
 }
 
