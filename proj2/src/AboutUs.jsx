3ecc0e3759fd1158ea59029b9547f3abd1fc6e60
rkumar06 <riyakumar06@gmail.com>
2024-04-01 21:17:47 -0700

    some about us stuff

--- proj2/src/AboutUs.jsx
+++ proj2/src/AboutUs.jsx
@@ -12,13 +12,13 @@ function AboutUs() {
 
                 <h3>Learn more about us and book us for your events! </h3>
                 <br/>
-                <h3>Who We are</h3>
+                <h3 className="about-title">Who We are</h3>
                 <p>blah blah blah</p>
                 <p>blah blah blah</p>
 
                 <div className = "about-events-flyers">
                     <div className = "about-events-text">
-                        <h3>Events</h3>
+                        <h3 className = "about-title">Events</h3>
                         <p>We organize Welcome Back to A Cappella, Cal’s a cappella orientation, at the beginning of every semester. Hundreds of students turn out to hear 13+ a cappella groups perform. The show is always fun, and we hope you’ll come!</p>
                     </div>
                     <div className = "about-flyers">
@@ -29,17 +29,18 @@ function AboutUs() {
 
                 <div className = "about-hire-us">
                     <div className = "about-hire-us-title">
+                        <h3 className = "about-title">Hire Us</h3>
                     </div> 
                     <div className = "about-hire-us-pic-paragraph">
-                        <p>We’re no stranger to weddings, private parties, tailgates, and conferences. We’ve performed at venues including San Francisco City Hall, University House, the Faculty Club, and UC Berkeley’s 2022 Commencement.</p>
-
+                        <img className = "about-hire-us-pic" src = "./src/images/hire-us-pic.jpg" />
+                        <p className = "about-hire-us-text">We’re no stranger to weddings, private parties, tailgates, and conferences. We’ve performed at venues including San Francisco City Hall, University House, the Faculty Club, and UC Berkeley’s 2022 Commencement.</p>
                     </div>
                     
 
                 </div>
                 <div className = "about-instagram">
                     <div className = "about-insta-title">
-
+                        <h3 className = "about-title">Follow Us</h3>
                     </div>
                     <div className = "insta-embed">
 
